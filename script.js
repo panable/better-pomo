e4ae567647bf38a79ba64baa1a2a2c99fe4f167a
Dhanveer Ramnauth <dhanr1606@gmail.com>
2026-08-04 21:22:28 +1000

    feat: add functional stopwatch

--- script.js
+++ script.js
@@ -2,15 +2,30 @@ window.onload = () => {
   console.log("Hello world");
   console.log(performance.now());
 
-  timer = document.getElementById("timer");
-  button = document.getElementById("button");
+  let button = document.getElementById("button");
+  let timer = document.getElementById("timer");
   button.addEventListener("click", startTimer);
-  secs = 0;
+  let secs = 0;
+  let now = null;
+  let timer_started = false;
+
+  // Maybe I can use timestamp instead of calculating current?
+  function tick(timestamp) {
+    if (!timer_started) return;
+    current = Date.now() - now;
+    let seconds = Math.floor(current / 1000);
+    let minutes = Math.floor(seconds / 60);
+
+    let visible_seconds = String(seconds % 60).padStart(2, "0");
+    let visible_minutes = String(minutes).padStart(2, "0");
+
+    timer.innerHTML = `${visible_minutes}:${visible_seconds}`;
+    requestAnimationFrame(tick);
+  }
 
   function startTimer() {
-    setTimeout(() => {
-      timer.innerHTML = `00:${secs++}`;
-      startTimer();
-    }, 1000);
+    now = Date.now();
+    timer_started = !timer_started;
+    requestAnimationFrame(tick);
   }
 };
